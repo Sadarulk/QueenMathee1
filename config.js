6ded6d5f83cfaf6f83865264764dd304c39779f0
BotTest001 <botdeploy179@gmail.com>
2024-10-17 20:56:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "3rADAZqR#b1NiFj3va2Cj4keMC6gnb4tR7qoROwRSxLay95IuKH0",
+SESSION_ID: process.env.SESSION_ID || "DrJV2bKB#3HQtZ1TY2s7MMBf2C8Dro6wBnMQp8fyi7o3cv1juzoI",
 MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",
 };
