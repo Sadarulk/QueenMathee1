97912aa8720a11f3a7d13b5387c469bc0740c5f1
BotTest001 <botdeploy179@gmail.com>
2024-10-18 21:59:58 +0530

    Add files via upload

---
+++ plugins/hirunews.js
@@ -0,0 +1,33 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "hirunews",
+    desc: "get hiru news",
+    category: "search",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/news/hiru`)
+
+let news = `*_QUEEN MATHEE HIRU NEWS 📰_*
+
+┌───────────────────
+├ • *Title:* ${data.result.title}
+├ • *News:* ${data.result.desc}
+├ • *Link:* ${data.result.url}
+└───────────────────
+
+> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+
+await conn.sendMessage(from,{image:{url: data.result.image},caption:news},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
