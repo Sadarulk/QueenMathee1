c50fa4ae208b5f192a4d63dfc5219b31a93a8286
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-22 21:38:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "bmZm1KwC#W1GxkvJu5-inBwmimnPPfK0GFd5Ixq5AvvpMdh0usJ8",
-MONGODB: process.env.MONGODB || "mongodb://mongo:XHVHnqUptVkdnPhgZyUExTisHaEfUSgL@autorack.proxy.rlwy.net:33494",
+MONGODB: process.env.MONGODB || "mongodb://mongo:vBzTgFNmkcAioMYxgpuaNGQbSRmvbisk@junction.proxy.rlwy.net:48886",
 };
