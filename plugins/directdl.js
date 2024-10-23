57fc8c588be684bee76c33a23725dc64ef34b8e9
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-23 16:59:35 +0530

    Update directdl.js

--- plugins/directdl.js
+++ plugins/directdl.js
@@ -11,7 +11,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let mvDl = ${q}
+let mvDl = `${q}`
     
 await conn.sendMessage(from,{document: {url:mvDl},mimetype:"video/mp4",fileName: + ".mp4",caption:"> ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡᴀ ʙᴏᴛ"},{quoted:mek})
     
