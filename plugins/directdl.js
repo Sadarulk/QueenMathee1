c788d8dc41b0b29d43dc77601092660e53c3fc22
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-23 13:14:44 +0530

    Add files via upload

---
+++ plugins/directdl.js
@@ -0,0 +1,22 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "dl",
+    desc: "download direct url",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let mvDL = `${q}`
+
+await conn.sendMessage(from,{document: {url:mvDL},mimetype:"video/mp4",movie + ".mp4",caption:"> ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡᴀ ʙᴏᴛ"},{quoted:mek})
+    
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
