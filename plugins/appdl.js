fb4bb1b7fee4aba1e061938ac8355cd8e43c504c
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-25 07:08:01 +0530

    Update appdl.js

--- plugins/appdl.js
+++ plugins/appdl.js
@@ -12,7 +12,7 @@ cmd({
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
 
-if (!q) return reply("*_Please give me a apk name._*")
+if (!q) return reply("*_Please give me a apk name_*")
 
         //fetch data from api  
         let data = await fetchJson(`https://api.maher-zubair.xyz/downloader/apk?apikey=d2d02440c70a98a415&q=${q}`)
