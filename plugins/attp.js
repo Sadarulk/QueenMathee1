2abbda608e70918c2fb3ad6ce7596e012aad8dce
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-26 21:19:59 +0530

    Add files via upload

---
+++ plugins/attp.js
@@ -0,0 +1,26 @@
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "attp",
+    desc: "attp sticker",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+if (!q) return reply("*_Please give me a text._*")
+
+        
+
+       await conn.sendMessage(from,{sticker: { url : https://api.fgmods.xyz/api/maker/attp?text=${q}&apikey=nRHt2lt5},package: 'QueenMathee'},{ quoted: mek })
+          
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
