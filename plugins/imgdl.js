4ac2c7e8424dc80eede0bdd9eea58c2f7f4dea01
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-26 20:33:50 +0530

    Update imgdl.js

--- plugins/imgdl.js
+++ plugins/imgdl.js
@@ -18,7 +18,11 @@ if (!q) return reply("*_Please give me a text._*")
         let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/download/piniimg?text=${q}`)
 
 
-        await conn.sendMessage(from,{image:{url:data.result.images_url}},{quoted:mek})
+        await conn.sendMessage(from,{image:{url:data.result[0].images_url}},{quoted:mek})
+        await conn.sendMessage(from,{image:{url:data.result[1].images_url}},{quoted:mek})
+        await conn.sendMessage(from,{image:{url:data.result[2].images_url}},{quoted:mek})
+        await conn.sendMessage(from,{image:{url:data.result[3].images_url}},{quoted:mek})
+        await conn.sendMessage(from,{image:{url:data.result[4].images_url}},{quoted:mek})
         
 
 }catch(e){
