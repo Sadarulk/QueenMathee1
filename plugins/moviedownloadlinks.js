84cccaa74762d5544c3b7fcf359c7f2481c465b0
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-27 08:45:43 +0530

    Update moviedownloadlinks.js

--- plugins/moviedownloadlinks.js
+++ plugins/moviedownloadlinks.js
@@ -17,43 +17,33 @@ const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
 
 const msg = `*_QUEEN MATHEE MOVIE DOWNLOAD LINKS_* 📥
 
-*Quality -* ${mv_info.result.data.dl_links[0].quality}
 *Size -* ${mv_info.result.data.dl_links[0].size}
 *Link -* ${mv_info.result.data.dl_links[0].link}
 
-*Quality -* ${mv_info.result.data.dl_links[1].quality}
 *Size -* ${mv_info.result.data.dl_links[1].size}
 *Link -* ${mv_info.result.data.dl_links[1].link}
 
-*Quality -* ${mv_info.result.data.dl_links[2].quality}
 *Size -* ${mv_info.result.data.dl_links[2].size}
 *Link -* ${mv_info.result.data.dl_links[2].link}
 
-*Quality -* ${mv_info.result.data.dl_links[3].quality}
 *Size -* ${mv_info.result.data.dl_links[3].size}
 *Link -* ${mv_info.result.data.dl_links[3].link}
 
-*Quality -* ${mv_info.result.data.dl_links[4].quality}
 *Size -* ${mv_info.result.data.dl_links[4].size}
 *Link -* ${mv_info.result.data.dl_links[4].link}
 
-*Quality -* ${mv_info.result.data.dl_links[5].quality}
 *Size -* ${mv_info.result.data.dl_links[5].size}
 *Link -* ${mv_info.result.data.dl_links[5].link}
 
-*Quality -* ${mv_info.result.data.dl_links[6].quality}
 *Size -* ${mv_info.result.data.dl_links[6].size}
 *Link -* ${mv_info.result.data.dl_links[6].link}
 
-*Quality -* ${mv_info.result.data.dl_links[7].quality}
 *Size -* ${mv_info.result.data.dl_links[7].size}
 *Link -* ${mv_info.result.data.dl_links[7].link}
 
-*Quality -* ${mv_info.result.data.dl_links[8].quality}
 *Size -* ${mv_info.result.data.dl_links[8].size}
 *Link -* ${mv_info.result.data.dl_links[8].link}
 
-*Quality -* ${mv_info.result.data.dl_links[9].quality}
 *Size -* ${mv_info.result.data.dl_links[9].size}
 *Link -* ${mv_info.result.data.dl_links[9].link}
 
