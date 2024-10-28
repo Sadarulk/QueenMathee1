57c2cfaf20b4a8d0d69af5cf58ad6a279a0b1bb7
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-28 21:34:45 +0530

    Update moviedownloadlinks.js

--- plugins/moviedownloadlinks.js
+++ plugins/moviedownloadlinks.js
@@ -5,198 +5,29 @@ const { cmd, commands } = require('../command')
 const apilink = 'https://dark-yasiya-api-new.vercel.app'
 
 cmd({
-    pattern: "mvd1",
+    pattern: "mvdlink",
     desc: "get movie download links",
+    category: "movie",
     filename: __filename
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
+  
     if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
 
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
+const mv = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
 
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[0].quality}\n\n*Size -* ${mv_info.result.data.dl_links[0].size}\n\n*Link -* ${mv_info.result.data.dl_links[0].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
+const array = mv.result.data.dl_links;
 
-}
-})
+for(let index = 0; index < array.length; index++) {
 
-cmd({
-    pattern: "mvd2",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
+const msg = `*Quality :* ${array[index].quality}\n\n*Size :* ${array[index].size}\n\n*Link -* ${array[index].link}`
+            
+await conn.sendMessage(from, { text: msg }, {quoted: mek})
 
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
+            }
 
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[1].quality}\n\n*Size -* ${mv_info.result.data.dl_links[1].size}\n\n*Link -* ${mv_info.result.data.dl_links[1].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd3",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[2].quality}\n\n*Size -* ${mv_info.result.data.dl_links[2].size}\n\n*Link -* ${mv_info.result.data.dl_links[2].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd4",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[3].quality}\n\n*Size -* ${mv_info.result.data.dl_links[3].size}\n\n*Link -* ${mv_info.result.data.dl_links[3].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd5",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[4].quality}\n\n*Size -* ${mv_info.result.data.dl_links[4].size}\n\n*Link -* ${mv_info.result.data.dl_links[4].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd6",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[5].quality}\n\n*Size -* ${mv_info.result.data.dl_links[5].size}\n\n*Link -* ${mv_info.result.data.dl_links[5].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd7",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[6].quality}\n\n*Size -* ${mv_info.result.data.dl_links[6].size}\n\n*Link -* ${mv_info.result.data.dl_links[6].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd8",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[7].quality}\n\n*Size -* ${mv_info.result.data.dl_links[7].size}\n\n*Link -* ${mv_info.result.data.dl_links[7].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd9",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[8].quality}\n\n*Size -* ${mv_info.result.data.dl_links[8].size}\n\n*Link -* ${mv_info.result.data.dl_links[8].link}`)
-        
-}catch(e){
-console.log(e)
-reply(`${e}`)
-
-}
-})
-
-cmd({
-    pattern: "mvd10",
-    desc: "get movie download links",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
-
-const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
-
-        return reply(`*Quality -* ${mv_info.result.data.dl_links[9].quality}\n\n*Size -* ${mv_info.result.data.dl_links[9].size}\n\n*Link -* ${mv_info.result.data.dl_links[9].link}`)
-        
+       
 }catch(e){
 console.log(e)
 reply(`${e}`)
