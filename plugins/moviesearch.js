6cfa390340a1669963501a433e63745103700150
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-28 18:35:23 +0530

    Update moviesearch.js

--- plugins/moviesearch.js
+++ plugins/moviesearch.js
@@ -17,28 +17,14 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
 const mv_list = await fetchJson(`${apilink}/movie/sinhalasub/search?text=${q}`)
 if(mv_list.result.length < 0) return await reply("*_Not results found !_*")
-
+        
+for(let index = 0; index < mv_list.length; index++);
+    
 const msg = `*_QUEEN MATHEE MOVIE SEARCH 🔎_*
 
-1.. *+ Title :* ${mv_list.result.data[0].title}
-    *+ Year :* ${mv_list.result.data[0].year}
-    *+ Link :* ${mv_list.result.data[0].link}
-
-2.. *+ Title :* ${mv_list.result.data[1].title}
-    *+ Year :* ${mv_list.result.data[1].year}
-    *+ Link :* ${mv_list.result.data[1].link}
-
-3.. *+ Title :* ${mv_list.result.data[2].title}
-    *+ Year :* ${mv_list.result.data[2].year}
-    *+ Link :* ${mv_list.result.data[2].link}
-
-4.. *+ Title :* ${mv_list.result.data[3].title}
-    *+ Year :* ${mv_list.result.data[3].year}
-    *+ Link :* ${mv_list.result.data[3].link}
-
-5.. *+ Title :* ${mv_list.result.data[4].title}
-    *+ Year :* ${mv_list.result.data[4].year}
-    *+ Link :* ${mv_list.result.data[4].link}
+${index} *+ Title :* ${mv_list.result.data[index].title}
+*+ Year :* ${mv_list.result.data[index].year}
+*+ Link :* ${mv_list.result.data[index].link}
 
 > ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
 
