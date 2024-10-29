2d0bc58ad0387387a42f7e7cc8bae591b8aa3ee0
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-29 09:14:06 +0530

    Update moviedownloadlinks.js

--- plugins/moviedownloadlinks.js
+++ plugins/moviedownloadlinks.js
@@ -2,6 +2,9 @@ const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
 
+let dt = `*_${array[index].title} DL links ⬇️_*\n\n`
+let cap = `\n\n> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+
 const apilink = 'https://dark-yasiya-api-new.vercel.app'
 
 cmd({
@@ -19,13 +22,11 @@ const mv = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
 
 const array = mv.result.data.dl_links;
 
-for(let index = 0; index < array.length; index++) {
-
-const msg = `*Quality :* ${array[index].quality}\n\n*Size :* ${array[index].size}\n\n*Link -* ${array[index].link}`
+const result = array.map((movie, index) => `${index + 1}. *Quality :* ${array[index].quality}\n\n*Size :* ${array[index].size}\n\n*Link :* ${array[index].link}`).join("\n\n");
             
-await conn.sendMessage(from, { text: msg }, {quoted: mek})
+await conn.sendMessage(from, { text: `${dt} ${result} ${cap}` }, {quoted: mek})
 
-            }
+            
 
 }catch(e){
 console.log(e)
