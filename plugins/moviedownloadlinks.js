dbe7765d2876861f23ea746af6a37be0c6cd17be
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-29 09:15:56 +0530

    Update moviedownloadlinks.js

--- plugins/moviedownloadlinks.js
+++ plugins/moviedownloadlinks.js
@@ -2,8 +2,7 @@ const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
 
-let dt = `*_${array[index].title} DL links ⬇️_*\n\n`
-let cap = `\n\n> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+
 
 const apilink = 'https://dark-yasiya-api-new.vercel.app'
 
@@ -20,7 +19,11 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
 
 const mv = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
 
+
 const array = mv.result.data.dl_links;
+
+let dt = `*_${array[index].title} DL links ⬇️_*\n\n`
+let cap = `\n\n> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
 
 const result = array.map((movie, index) => `${index + 1}. *Quality :* ${array[index].quality}\n\n*Size :* ${array[index].size}\n\n*Link :* ${array[index].link}`).join("\n\n");
             
