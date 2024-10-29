aa5651b37e417665931d067f54b39d5088af44e8
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-29 10:17:30 +0530

    Update tvshowinfo.js

--- plugins/tvshowinfo.js
+++ plugins/tvshowinfo.js
@@ -3,6 +3,7 @@ const config = require('../config')
 const { cmd, commands } = require('../command')
 
 const apilink = 'https://dark-yasiya-api-new.vercel.app'
+let cap = `\n\n> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
 
 cmd({
     pattern: "tvshow",
@@ -12,21 +13,35 @@ cmd({
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-    if (!q && !q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
+    if (!q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
 
 const ts = await fetchJson(`${apilink}/movie/sinhalasub/tvshow?url=${q}`)
 
 
         const array = ts.result.data.episodes;
 
-	for(let index = 0; index < array.length; index++){
+	    const msg = `*_QUEEN MATHEE TVSHOW DETAILS_* 🔎
 
-const msg = `*S & E :* ${array[index].title}\n\n*Date :* ${array[index].date}\n\n*Link :* ${array[index].episode_link}`
+🍟 *TV Show Name :* ${ts.result.data.title}
 
-await conn.sendMessage(from, { text: msg }, {quoted: mek})
+🧿 *Release Date :* ${ts.result.data.date}
+
+🎀 *Categories :* ${ts.result.data.category}
+
+⭐ *IMDB Rate :* ${ts.result.data.imdb}
+
+🤵‍♂ *Director* : ${ts.result.data.director}
+
+📝 *Description :* ${ts.result.data.desc}\n\n\n`
+
+	
+
+const result = array.map((movie, index) => `*Season & Episode :* ${array[index].title}\n\n*Date :* ${array[index].date}\n\n*Link :* ${array[index].episode_link}`).join("\n\n");
+		
+await conn.sendMessage(from, { text: `${msg} ${result} ${cap}` }, {quoted: mek})
 
 
-}
+
 
 }catch(e){
 console.log(e)
