652abb07739b526c2a06055f85a6a8a412f49853
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-30 21:51:46 +0530

    Update forwardtest.js

--- plugins/forwardtest.js
+++ plugins/forwardtest.js
@@ -17,20 +17,20 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               let res = await axios.head(url)
               mime = res.headers['content-type']
               if (mime.split("/")[1] === "gif") {
-                return conn.sendMessage(${q}, { video: await getBuffer(url), caption: caption, gifPlayback: true, ...options }, { quoted: quoted, ...options })
+                return conn.sendMessage(`${q}`, { video: await getBuffer(url), caption: caption, gifPlayback: true, ...options }, { quoted: quoted, ...options })
               }
               let type = mime.split("/")[0] + "Message"
               if (mime === "application/pdf") {
-                return conn.sendMessage(${q}, { document: await getBuffer(url), mimetype: 'application/pdf', caption: caption, ...options }, { quoted: quoted, ...options })
+                return conn.sendMessage(`${q}`, { document: await getBuffer(url), mimetype: 'application/pdf', caption: caption, ...options }, { quoted: quoted, ...options })
               }
               if (mime.split("/")[0] === "image") {
-                return conn.sendMessage(${q}, { image: await getBuffer(url), caption: caption, ...options }, { quoted: quoted, ...options })
+                return conn.sendMessage(`${q}`, { image: await getBuffer(url), caption: caption, ...options }, { quoted: quoted, ...options })
               }
               if (mime.split("/")[0] === "video") {
-                return conn.sendMessage(${q}, { video: await getBuffer(url), caption: caption, mimetype: 'video/mp4', ...options }, { quoted: quoted, ...options })
+                return conn.sendMessage(`${q}`, { video: await getBuffer(url), caption: caption, mimetype: 'video/mp4', ...options }, { quoted: quoted, ...options })
               }
               if (mime.split("/")[0] === "audio") {
-                return conn.sendMessage(${q}, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
+                return conn.sendMessage(`${q}`, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
 
