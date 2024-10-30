b34b8315a0bf01ddb11e3ba0d36b015124b26f21
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-31 00:08:36 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -8,11 +8,11 @@ cmd({
     category: "other",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+async(conn, mek, m,{from, fromm, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
 
-await conn.updateBlockStatus("${q}", "block") // Block user
+await conn.updateBlockStatus("mek.key.remoteJid", "block") // Block user
 
 
     
