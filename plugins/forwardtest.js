1186d8ce01467277cf7670709888becbcf95f03d
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-10-30 22:30:49 +0530

    Update forwardtest.js

--- plugins/forwardtest.js
+++ plugins/forwardtest.js
@@ -11,9 +11,9 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const msg = `*_This is forward test msg..._*`
 
-await conn.sendMessage('${q}', { forward: msg })
+
+await conn.sendMessage('${q}', { forward: `*_This is forward test msg..._` })
 
 }catch(e){
 console.log(e)
