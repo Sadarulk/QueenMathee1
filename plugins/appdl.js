5a2d4683c524310613f90b18bbcd4c4e655b4e75
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-08 11:56:48 +0530

    Update appdl.js

--- plugins/appdl.js
+++ plugins/appdl.js
@@ -1,6 +1,8 @@
-const { fetchJson } = require('../lib/functions'); // Assuming this is a helper function for fetch requests
-const config = require('../config');
-const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+
 
 cmd({
     pattern: "apk",
@@ -8,42 +10,32 @@ cmd({
     desc: "download apk",
     category: "download",
     filename: __filename
-}, 
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, reply }) => {
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        if (!q) return reply("*_Please give me an APK name._*");
 
-        // Fetch data from API
-        const apk = await fetchJson(`https://api.nexoracle.com/downloader/apk?apikey=d2d02440c70a98a415&q=${q}`);
+if (!q) return reply("*_Please give me a apk name._*")
 
-        if (!apk || !apk.result) {
-            return reply("*_Sorry, I couldn't find the APK you're looking for._*");
-        }
+        //fetch data from api  
+        const apk = await fetchJson(`https://api.nexoracle.com/downloader/apk?apikey=d2d02440c70a98a415&q=${q}`)
 
-        const msg = `*_QUEEN MATHEE APK DOWNLOADER 📥_*
+const msg = `*_QUEEN MATHEE APK DOWNLOADER 📥_*
 
 *■ App name:* ${apk.result.name}
 *■ Last update:* ${apk.result.lastup}
 *■ Size:* ${apk.result.size}
 *■ Package:* ${apk.result.package}
 
-> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`;
+> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
 
-        // Send APK information with icon
-        await conn.sendMessage(from, { 
-            image: { url: apk.result.icon }, 
-            caption: msg 
-        }, { quoted: mek });
+        await conn.sendMessage(from,{image:{url: apk.result.icon},caption:msg},{quoted:mek})
+        //send apk
+        await conn.sendMessage(from,{document: {url: apk.result.dllink},mimetype:"application/vnd.android.package-archive",fileName: apk.result.name + ".apk"},{quoted:mek})
+         
 
-        // Send the APK file
-        await conn.sendMessage(from, {
-            document: { url: apk.result.dllink },
-            mimetype: "application/vnd.android.package-archive",
-            fileName: `${apk.result.name}.apk`
-        }, { quoted: mek });
+}catch(e){
+console.log(e)
+reply(`${e}`)
 
-    } catch (e) {
-        console.log(e);
-        reply(`${e}`);
-    }
-});
+}
+})
