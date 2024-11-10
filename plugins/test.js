1d3b8ea4ccd832b6aeccb2a095ff2957b1235674
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-10 13:33:10 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -3,16 +3,14 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "test",
-    desc: "Check bot online or no.",
+    desc: "test",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let msg = `*_Hi, This is Queen Mathee Test Msg._*`
-  
-  await conn.sendMessage(120363362209453987@g.us,{text: ${msg} },{quoted: mek})
+ await conn.sendMessage(`94741545187@s.whatsapp.net`,{text: `*_Hi, This is Queen Mathee Test Msg._*` })
   
 }catch(e){
 console.log(e)
