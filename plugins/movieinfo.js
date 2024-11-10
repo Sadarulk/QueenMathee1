b7904891a06b4383f86913fff0f0e9cab2f86d3d
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-10 14:54:40 +0530

    Update movieinfo.js

--- plugins/movieinfo.js
+++ plugins/movieinfo.js
@@ -3,6 +3,7 @@ const config = require('../config')
 const { cmd, commands } = require('../command')
 
 const apilink = 'https://dark-yasiya-api-new.vercel.app'
+const id = `120363355439809658@g.us`
 
 cmd({
     pattern: "movie2",
@@ -37,6 +38,44 @@ const msg = `*_QUEEN MATHEE MOVIE DETAILS_* 🔎
 > ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
 
 await conn.sendMessage(from,{image:{url: mv_info.result.data.images[0]},caption:msg},{quoted:mek})  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
+cmd({
+    pattern: "msend",
+    desc: "get movie info",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+    if (!q.startsWith("https://")) return reply("*_Please give me a sinhalasub.lk url._*")
+
+const mv_info = await fetchJson(`${apilink}/movie/sinhalasub/movie?url=${q}`)
+
+const msg = `🍟 *Movie Name :* ${mv_info.result.data.title}
+
+🧿 *Release Date :* ${mv_info.result.data.date}
+
+🌍 *Country :* ${mv_info.result.data.country}
+
+⏱ *Duration :* ${mv_info.result.data.runtime}
+
+🎀 *Categories :* ${mv_info.result.data.category}
+
+⭐ *IMDB Rate :* ${mv_info.result.data.imdbRate}
+
+🔮 *IMDB Count:* ${mv_info.result.data.imdbVoteCount}
+
+🤵‍♂ *Director* : ${mv_info.result.data.director}
+
+> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+
+await conn.sendMessage( id ,{image:{url: mv_info.result.data.images[0]},caption:msg})  
 
 }catch(e){
 console.log(e)
