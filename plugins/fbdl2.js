b01bdb37d6e766f8d4febb45520c655e06704a5c
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-11 13:46:13 +0530

    Update fbdl2.js

--- plugins/fbdl2.js
+++ plugins/fbdl2.js
@@ -17,6 +17,12 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
         //fetch data from api  
         let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/download/fbdl1?url=${q}`)
 
+        const array = data.result;
+        
+        if (!array || array.length === 0) {
+            return reply("*_Can't find anything._*");
+        }
+
          let fb = `*_QUEEN MATHEE FACEBOOK DOWNLOADER 📥_*
 
 🖇️ Url : ${data.result.url}
@@ -38,3 +44,48 @@ Your facebook video is downloading...
 
     }
     })
+
+cmd({
+    pattern: "tt2",
+    alias: ["tiktok2"],
+    desc: "download tt videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("*_Please give me a tiktok url._*")
+        //fetch data from api  
+        let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/download/tiktok?url=${q}`)
+
+         const array = data.result;
+        
+        if (!array || array.length === 0) {
+            return reply("*_Can't find anything._*");
+        }
+        
+let tt = `*_QUEEN MATHEE TIKTOK DOWNLOADER 📥_*
+
+● Title : ${data.result.title}
+
+● Author : ${data.result.author}
+
+● Duration : ${data.result.duration}
+
+● Url : ${q}
+
+> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+
+        
+        await conn.sendMessage(from,{image:{url:"https://github.com/Sadarulk/QueenMatheeDB/blob/main/botlogos/tiktok.jpg?raw=true"},caption:tt},{quoted:mek})
+        
+        await conn.sendMessage(from, { video: { url: data.result.hdVideo }, mimetype: "video/mp4", caption: `*without WaterMark*\n\n${cap}` }, { quoted: mek })
+        await conn.sendMessage(from, { video: { url: data.result.wmVideo }, mimetype: "video/mp4", caption: `*with WaterMark*\n\n${cap}` }, { quoted: mek }) 
+        await conn.sendMessage(from, { audio: { url: data.result.sound }, mimetype: "mpeg/mp3"}, { quoted: mek })
+        
+
+} catch (e) {
+console.log(e);
+reply(`${e}`);
+}
+})
