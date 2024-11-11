67cb7e24fcf3a0f130d718d319edc71ae77ab5d8
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-11 13:17:56 +0530

    Add files via upload

---
+++ plugins/fbdl2.js
@@ -0,0 +1,40 @@
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+let cap = '> ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡᴀ ʙᴏᴛ'
+
+cmd({
+    pattern: "fb2",
+    alias: ["facebook2"],
+    desc: "download fb videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("*_Please give me a facebook url._*")
+        //fetch data from api  
+        let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/download/fbdl1?url=${q}`)
+
+         let fb = `*_QUEEN MATHEE FACEBOOK DOWNLOADER 📥_*
+
+🖇️ Url : ${data.result.url}
+
+Your facebook video is downloading...
+
+> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
+        
+        await conn.sendMessage(from,{image:{url: data.result.thumbnail },caption:fb},{quoted:mek})
+
+        //send video (hd,sd)
+
+        await conn.sendMessage(from,{video: {url: data.result.hd},mimetype:"video/mp4",caption: `*HD QUALITY*\n\n${cap}` },{quoted: mek})
+        await conn.sendMessage(from,{video: {url: data.result.sd},mimetype:"video/mp4",caption: `*SD QUALITY*\n\n${cap}` },{quoted: mek})  
+
+        }catch(e){
+        console.log(e)
+        reply(`${e}`)
+
+    }
+    })
