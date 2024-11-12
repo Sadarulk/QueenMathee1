c38b28384634a34f4c434211903a2b0ca12f6305
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-12 21:05:15 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,28 +1,27 @@
 const { fetchJson } = require('../lib/functions')
 const config = require('../config')
 const { cmd, commands } = require('../command')
+const { MessageType, MessageOptions, Mimetype } = require('@whiskeysockets/baileys')
 
 
 cmd({
-    pattern: "dev",
+    pattern: "developer",
     desc: "owner details",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-    
-   let msg = `🧑‍💻 *_Owner & Developer :_* Sadaru
 
-🪀 *_WhatsApp :_* wa.me/+94701814946
-
-🌐 *_From :_* Kurunegala
-
-📌 *_Student of :_* M.C.C
-
-> ǫᴜᴇᴇɴ ᴍᴀᴛʜᴇᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ`
-
-return reply(`${msg}`)
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Lakmal Sadaru\n' // full name
+            + 'ORG:Infinity SL;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=94701814946:+94 7018 14946\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+        
+await conn.sendMessage(from,{ contacts: { displayName: 'Sadaru', contacts: [{ vcard }] }})
+        
         
 }catch(e){
 console.log(e)
