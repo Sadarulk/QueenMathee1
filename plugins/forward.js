e98ce899d196c26404ab93505aa2f2a0e2cd9ccd
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-12 22:13:49 +0530

    Update forward.js

--- plugins/forward.js
+++ plugins/forward.js
@@ -12,8 +12,22 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
     try {
 
 	if(!isOwner) return
-    
-await conn.sendMessage(q, {forward: quoted })
+
+    const isQuotedViewOnce = m.quoted ? (m.quoted.type === 'viewOnceMessage') : false
+    const isQuotedImage = m.quoted ? ((m.quoted.type === 'imageMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'imageMessage') : false)) : false
+    const isQuotedVideo = m.quoted ? ((m.quoted.type === 'videoMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'videoMessage') : false)) : false
+    const isQuotedAudio = m.quoted ? ((m.quoted.type === 'audioMessage') || (isQuotedViewOnce ? (m.quoted.msg.type === 'audioMessage') : false)) : false
+    const isQuotedSticker = m.quoted ? (m.quoted.type === 'stickerMessage') : false
+
+	    if ((m.type === 'imageMessage') || isQuotedImage) {
+		    
+		    await conn.sendMessage(q, {image: m.quoted})
+
+}  else if ( isQuotedVideo ) { 
+
+ await conn.sendMessage(q, {video: m.quoted})
+
+}else return await  reply(`*_This message can't forward._*`)
 
 }catch(e){
 console.log(e)
