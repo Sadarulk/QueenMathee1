9b9cf7e9d8fab5b672d948b72168e56c5800707e
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-12 21:09:51 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,11 +1,9 @@
-const { fetchJson } = require('../lib/functions')
-const config = require('../config')
 const { cmd, commands } = require('../command')
 const { MessageType, MessageOptions, Mimetype } = require('@whiskeysockets/baileys')
 
 
 cmd({
-    pattern: "developer",
+    pattern: "owner",
     desc: "owner details",
     category: "main",
     filename: __filename
@@ -14,9 +12,9 @@ async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, send
     try {
 
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
-            + 'VERSION:3.0\n' 
+            + 'VERSION:4.0\n' 
             + 'FN:Lakmal Sadaru\n' // full name
-            + 'ORG:Infinity SL;\n' // the organization of the contact
+            + 'ORG:Infinity WA Bot;\n' // the organization of the contact
             + 'TEL;type=CELL;type=VOICE;waid=94701814946:+94 7018 14946\n' // WhatsApp ID + phone number
             + 'END:VCARD'
         
