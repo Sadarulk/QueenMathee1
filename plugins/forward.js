9bc2a8f42703c9269951c47abf160db47219da55
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-12 20:31:22 +0530

    Update and rename testvcard.js to forward.js

---
+++ plugins/forward.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "send",
+    alias: ["forward","fw"],
+    desc: "forward msg",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+	if(!isOwner) return
+    
+await conn.sendMessage(q, {forward: m.quoted })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
