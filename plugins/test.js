806cce696c57a7b44fd495b1c0caf4d5277d22c2
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-13 12:11:14 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,7 +2,7 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "tttt",
+    pattern: "test",
     desc: "test cmd",
     category: "other",
     filename: __filename
@@ -10,11 +10,21 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const msg = mek 
-const id = '120363292156762185@g.us'
+const msg = function checkNumber(q) {
+    if (101 > q >= 75) {
+        return "A";
+    } else if (75 > q >= 65) {
+        return "B";
+    } else if (65 > q >= 55) {
+        return "C";
+    } else if (55 > q >= 35) {
+        return "S";
+    } else {
+        return "W";
+    }
+}
 
-
-await conn.sendMessage(id,{text: msg})
+await conn.sendMessage(from,{text: msg})
 
 
 }catch(e){
