d773cd7e9f832e4b40aec729d668e76762c8fb1c
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-13 12:20:48 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -10,19 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-function checkNumber(q) {
-    if (101 > q >= 75) {
-        return reply("A");
-    } else if (75 > q >= 65) {
-        return reply("B");
-    } else if (65 > q >= 55) {
-        return reply("C");
-    } else if (55 > q >= 35) {
-        return reply("S");
-    } else {
-        return reply("W");
-    }
-}
+if (q > 75) return reply(`A`)
 
 }catch(e){
 console.log(e)
