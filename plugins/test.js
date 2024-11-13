f0a504c6cdfb826bedd178b4c367c9e489fb27b7
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-13 10:11:41 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "test",
+    desc: "test cmd",
+    category: "other",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const msg = quoted 
+const id = '120363292156762185@g.us'
+
+await conn.sendMessage( id , {text: msg})
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
