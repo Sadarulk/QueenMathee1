24474b224f09a6df0e4ecaf6a0a346705177da27
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-11-14 08:38:14 +0530

    Update and rename newstest.js to sttsviews.js

---
+++ plugins/sttsviews.js
@@ -0,0 +1,44 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+
+cmd({
+    pattern: "sttsviews",
+    desc: "Stts views contact details",
+    category: "other",
+    filename: __filename
+},
+ 
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let a = q
+
+let b = a.split("&")
+
+let c = b[0]
+let d = b[1]
+let f = b[2]
+
+let msg = `Ｆᴏʀ ＳᴛΔᵀᴜs Ｖɪᴇᴡs "🙇🏻‍♀️❤️🖇️
+
+*⭕Name :* ${c}
+
+*⭕From :* ${d}
+
+*⭕Age :* ${f}
+  
+https://wa.me/${senderNumber}?text=Hii+${c}+(Stts+views)
+
+*_⭕save daganna hode_* ✍️🙈💬
+
+♡ ㅤ      ❍ㅤ        ⎙ㅤ       ⌲`
+
+await conn.sendMessage(from ,{text: msg},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+}
